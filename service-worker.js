3c520c5ac5906528b1f65119b11234b216f3785a
haniwa57 <p6614175@gmail.com>
2024-11-23 12:55:45 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,22 @@
-const CACHE_NAME = 'countdown-timer-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('countdown-timer-v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/style.css',
+                '/manifest.json',
+                '/icon-192x192.png',
+                '/icon-512x512.png',
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
